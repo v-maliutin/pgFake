28881bda9008e828df1edda03b35303fc8dbd105
mslava <vaychislav.malyutin@gmail.com>
2026-02-01 22:09:16 +0700

    Implemented parameter logging inside mocked functions

--- pgsqlmock--1.0.0.sql
+++ pgsqlmock--1.0.0.sql
@@ -33,6 +33,33 @@ exception when others then
 end;
 $$ language plpgsql stable;
 
+create FUNCTION _param_of_mode(
+	_param_modes text[], 
+	_param_names text[], 
+	_mode text default 'IN'
+)
+ RETURNS setof text
+ LANGUAGE plpgsql
+ STABLE
+AS $function$
+declare
+	_type text;	
+begin
+	if _mode = 'IN' then
+		_type = 'i';
+	end if;
+
+return query 
+	select pn
+	from(
+		select _param_modes[i] as pm, _param_names[i] as pn
+		from generate_series(1, cardinality(_param_names)) as i
+	) as tt
+	where tt.pm = _type or _param_modes is null;
+end;
+$function$
+;
+
 create view tap_funky_ext as
 select
 	tf.oid,
@@ -54,10 +81,15 @@ select
 	format('(%s)',
 		routine_info.args_with_defs
 	)                                        AS args_with_defs,
-	format('raise notice ''%s'', %s;', 
-		coalesce(param_names.placeholders, params.placeholders),
-		params.dollars
-	)                                        as raise_notice
+	case 
+		when nullif(routine_info.args_with_defs, '') is not null then
+			format('raise notice ''%s'', %s;', 
+				coalesce(param_names.placeholders, param.placeholders),
+				param.dollars
+			)
+		else '' 
+	end                                      as param_notice,
+	param.dollars
 from tap_funky tf
 join pg_catalog.pg_proc p on p.oid = tf.oid
 join pg_catalog.pg_namespace n ON p.pronamespace = n.oid
@@ -84,9 +116,10 @@ left join lateral (
 		string_agg('$' || i, ', ') dollars,
 		string_agg('%', ', ') placeholders
 	from generate_series(1, p.pronargs) as i
-) params on true
+) param on true
 left join lateral (
-	select string_agg(pn || ' = %', ', ') placeholders from unnest(p.proargnames) as pn
+	select string_agg(pn || ' = %', ', ') placeholders 
+	from _param_of_mode(p.proargmodes, p.proargnames, 'IN') as pn
 ) param_names on true;
 
 
@@ -123,6 +156,7 @@ create function mock_func(
     , _func_args text
     , _return_set_value text default null
     , _return_scalar_value anyelement default null::text
+	, _log_params bool default null
 )
 returns void
 --creates a mock in place of a real function
@@ -136,11 +170,13 @@ declare
 	_returns_set bool;
 	_variants text;
 	_ex_msg text;
+	_param_notice text;
+	_dollars text;
 begin
 	--First of all, we have to identify which function we must mock. If there is no such function, throw an error.
 	begin
-	    select "returns", langname, returns_set
-	    into strict _func_result_type, _func_language, _returns_set
+	    select "returns", langname, returns_set, case when _log_params then param_notice else '' end, dollars
+	    into strict _func_result_type, _func_language, _returns_set, _param_notice, _dollars
 	    from tap_funky_ext
 	    where "schema" = _func_schema
 	        and "name" = _func_name
@@ -173,12 +209,15 @@ begin
 		_mock_ddl = format('
 	        create or replace function %1$I.%2$I %3$s
 	             returns %4$s
-	             language %5$s
-	        AS %7$sfunction%7$s
-	            select * from %1$I.__%2$I ( ''%6$s'' );
-	        %7$sfunction%7$s;',
+	             language plpgsql
+	        AS %6$sfunction%6$s
+			begin
+				%7$s
+	            return query select * from %1$I.__%2$I ( ''%5$s'' );
+			end;
+	        %6$sfunction%6$s;',
 			_func_schema/*1*/, _func_name/*2*/, _func_args/*3*/, _func_result_type/*4*/,
-			_func_language/*5*/, _return_set_value/*6*/, '$'/*7*/);
+			_return_set_value/*5*/, '$'/*6*/, _param_notice/*7*/);
 	    execute _mock_ddl;
 	end if;
 
@@ -189,24 +228,54 @@ begin
 	             language plpgsql
 	        AS %6$sfunction%6$s
 			begin
+				%7$s
 	            return query execute _query( ''%5$s'' );
 			end;
 	        %6$sfunction%6$s;',
 			_func_schema/*1*/, _func_name/*2*/, _func_args/*3*/, _func_result_type/*4*/,
-			_return_set_value/*5*/, '$'/*6*/);
+			_return_set_value/*5*/, '$'/*6*/, _param_notice/*7*/);
 	    execute _mock_ddl;
 	end if;
 
 	if not _returns_set then
-		_mock_ddl = format('
-	        create or replace function %1$I.%2$I %3$s
-	             RETURNS %4$s
-	             LANGUAGE %5$s
-	        AS %8$sfunction%8$s
-	            select %6$L::%7$s;
-	        %8$sfunction%8$s;',
-			_func_schema/*1*/,  _func_name/*2*/, _func_args/*3*/, _func_result_type/*4*/,
-			_func_language/*5*/, _return_scalar_value/*6*/, pg_typeof(_return_scalar_value)/*7*/, '$'/*8*/);
+		if _func_language = 'plpgsql' then
+			_mock_ddl = format('
+				create or replace function %1$I.%2$I %3$s
+					RETURNS %4$s
+					LANGUAGE %5$s
+				AS %8$sfunction%8$s
+					%9$s
+					return %6$L::%7$s;
+				%8$sfunction%8$s;',
+				_func_schema/*1*/,  _func_name/*2*/, _func_args/*3*/, _func_result_type/*4*/,
+				_func_language/*5*/, _return_scalar_value/*6*/, pg_typeof(_return_scalar_value)/*7*/, '$'/*8*/,
+				_param_notice/*9*/);
+			execute _mock_ddl;
+		else
+			_mock_ddl = format('
+				create function %1$I.__%2$I %3$s
+					returns %4$s
+					language plpgsql
+				AS %5$sfunction%5$s
+				begin
+					%6$s
+					return %7$L::%8$s;
+				end;
+				%5$sfunction%5$s;',
+				_func_schema/*1*/, _func_name/*2*/, _func_args/*3*/, _func_result_type/*4*/, '$'/*5*/, _param_notice/*6*/,
+				_return_scalar_value/*7*/, pg_typeof(_return_scalar_value)/*8*/);
+			execute _mock_ddl;
+			_mock_ddl = format('
+				create or replace function %1$I.%2$I %3$s
+					RETURNS %4$s
+					LANGUAGE %5$s
+				AS %6$sfunction%6$s
+					select %1$I.__%2$I (%7$s);
+				%6$sfunction%6$s;',
+				_func_schema/*1*/,  _func_name/*2*/, _func_args/*3*/, _func_result_type/*4*/,
+				_func_language/*5*/, '$'/*6*/, _dollars/*7*/);
+			execute _mock_ddl;
+		end if;
 	    execute _mock_ddl;
 	end if;
 end $function$;
